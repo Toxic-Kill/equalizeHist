184fbd0b6ad754196dbd9e760dc1c2dce8e85ab7
Kill Toxic <913087364@qq.com>
2020-05-15 16:18:12 +0800

    equalizeHist

--- equalizeHist/equalizeHist.cpp
+++ equalizeHist/equalizeHist.cpp
@@ -1,20 +1,34 @@
-﻿// equalizeHist.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include <iostream>
+#include<opencv2/opencv.hpp>
 
-#include <iostream>
+using namespace std;
+using namespace cv;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	cv::Mat dstMat;
+	//读取图像
+	cv::Mat srcMat = cv::imread("D:\\Files\\etest.jpg");
+	//检查读取成功性
+	if (srcMat.empty())
+	{
+		std::cout << "Fail to open" << std::endl;
+		return -1;
+	}
+
+	//分离三通道
+	std::vector<cv::Mat>channels;
+	cv::split(srcMat, channels);
+
+	//分别进行直方图均衡
+	cv::equalizeHist(channels[0], channels[0]);
+	cv::equalizeHist(channels[1], channels[1]);
+	cv::equalizeHist(channels[2], channels[2]);
+
+	//合成目标图像
+	cv::merge(channels, dstMat);
+
+	//显示图像
+	cv::imshow("dst", dstMat);
+	waitKey(0);
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
